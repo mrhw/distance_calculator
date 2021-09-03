ac21929a6e8c2cd1caf85227ddf859f14d57a091
jakub_marchwinski <jakub.marchwinski@gmail.com>
2021-09-03 17:00:31 +0200

    Set default LengthUnit in case request is not correct;

--- src/main/java/com/mrhw/distancecalulator/model/Request.java
+++ src/main/java/com/mrhw/distancecalulator/model/Request.java
@@ -1,12 +1,16 @@
 package com.mrhw.distancecalulator.model;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 
 @JsonAutoDetect
 @Data
 public class Request {
+    @JsonIgnore(value = false)
     private Distance firstDistance;
+    @JsonIgnore(value = false)
     private Distance secondDistance;
-    private LengthUnit resultUnit;
+    @JsonIgnore(value = false)
+    private LengthUnit resultUnit = LengthUnit.M;
 }
